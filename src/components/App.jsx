94b65dc7165188adf3be9f5b4b52e5dc93151819
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-02-20 22:30:13 +0600

    scroll to top added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { BrowserRouter as Router } from "react-router-dom";
 import Fetch from "./pages/Fetch";
+import ScrollToTop from "./ScrollToTop";
 
 function App(){
     return(
         <div>
         <Router>
+        <ScrollToTop />
         <Header />
         <Fetch />
         <Footer />
